71ab1bafb53c2457ffe2e5bed7c6a6bfd92cf31e
mollyexten <molly.exten@gmail.com>
2021-04-26 15:27:50 -0400

    define authorize_reqeust in ApplicationController, call authorize_request in before_action of UsersController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,23 @@ class ApplicationController < ActionController::API
     decoded = JWT.decode(token, SECRET_KEY)[0]
     HashWithIndifferentAccess.new decoded
   end
+
+  # As long as the user exists, any controller that calls this method will get present user data
+  def authorize_request
+    # Grabs the header and splits out the token from the header
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    begin
+      # Decode helper method pulls user info from the token
+      @decoded = decode(header)
+      # Set isntance variable current_user using user_id from token data
+      @current_user = User.find(@decoded[:id])
+    # If the user can't be found or the token isn't valid, we raise an unauthorized error
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
+  end
+
 end
