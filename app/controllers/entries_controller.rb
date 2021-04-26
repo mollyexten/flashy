d669a416161373646647a197a985a70542fb078a
mollyexten <molly.exten@gmail.com>
2021-04-26 17:15:27 -0400

    created route and controller methods for getting all entries for a deck

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,11 @@
 class EntriesController < ApplicationController
+  before_action :set_deck
+  def index
+    @entries = Entry.where(deck_id: @deck)
+    render json: @entries, include: :deck, status: :ok
+  end
+  private
+  def set_deck
+    @deck = Deck.find(params[:deck_id])
+  end
 end
