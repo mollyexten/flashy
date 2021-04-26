69c43b8f2c944222aa5251de31f62ca6027167ba
mollyexten <molly.exten@gmail.com>
2021-04-26 17:10:26 -0400

    added index, show, and create routes and controller methods for deck table

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,2 +1,24 @@
 class DecksController < ApplicationController
+  def index
+    @decks = Deck.all
+    render json: @decks
+  end
+  def show
+    @deck = Deck.find(params[:id])
+    render json: @deck
+  end
+  def create
+    @deck = Deck.new(deck_params)
+    if @deck.save
+      render json: @deck, status: :created
+    else
+      render json: @deck.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def deck_params
+    params.require(:deck).permit(:title, :user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   post '/auth/login', to: 'authentication#login'
   get '/auth/verify', to: 'authentication#verify'
   resources :users
+  resources :decks, only: [:index, :show, :create] do
+    resources :entries, only: [:index, :create]
+  end
+  resources :entries, only: [:show, :update, :destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
