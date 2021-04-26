44bf48fef8bbfb76af68d06e61cadd3478602dde
mollyexten <molly.exten@gmail.com>
2021-04-26 15:10:34 -0400

    include token in response for user post method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,12 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      # Include token in response whenever a user registers, but don't send password_digest
+      @token = encode({id: @user.id})
+      render json: {
+        user: @user.attributes.except("password_digest"),
+        token: @token
+        }, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
