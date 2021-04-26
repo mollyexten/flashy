bf780430c462183338bcffc71f49df418d2813fa
mollyexten <molly.exten@gmail.com>
2021-04-26 17:19:38 -0400

    created route and controller methods for entry post request

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,8 +4,19 @@ class EntriesController < ApplicationController
     @entries = Entry.where(deck_id: @deck)
     render json: @entries, include: :deck, status: :ok
   end
+  def create
+    @entry = Entry.new(entry_params)
+    if @entry.save
+      render json: @entry, status: :created
+    else
+      render json: @entry.errors, status: :unprocessable_entity
+    end
+  end
   private
   def set_deck
     @deck = Deck.find(params[:deck_id])
+  end
+  def entry_params
+    params.require(:entry).permit(:term, :details, :deck_id)
   end
 end
