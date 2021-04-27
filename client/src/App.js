f5b1276d159d15c669e416318acf7f24656dd067
mollyexten <molly.exten@gmail.com>
2021-04-26 20:27:20 -0400

    installed axios and react-router-dom, routed app.js to decks screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,33 @@
 import './App.css';
+import { useState, useEffect } from "react";
+import { Route, Switch } from "react-router-dom";
+import Decks from "./screens/Decks/Decks"
 
 function App() {
+  const [user, setUser] = useState(null)
+
+  // useEffect to fetch user here
+  // userEffect(() => {
+  //   const fetchUser = async () => {
+  //     const user = await verifyUser()
+  //     user ? setUser(user) : setUser(null)
+  //   }
+  //   fetchUser()
+  // }, [])
+
+  // clearUser here
+  // const clearUser = () => {
+  //   localStorage.removeItem('token')
+  //   setUser(null)
+  // }
+
   return (
     <div className="App">
+      <Switch>
+        <Route exact path="/">
+          <Decks />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ client/src/screens/Decks/Decks.jsx
@@ -0,0 +1,5 @@
+export default function Decks(props) {
+  return (
+    <div>DECKS SCREEN</div>
+  )
+}
