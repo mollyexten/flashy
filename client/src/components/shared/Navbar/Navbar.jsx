3ebe6880c274798bb3d8c0ec8fc07b67d3c0e180
mollyexten <molly.exten@gmail.com>
2021-04-26 21:57:55 -0400

    added basic signup form to signup screen

--- client/src/components/shared/Navbar/Navbar.jsx
+++ client/src/components/shared/Navbar/Navbar.jsx
@@ -1,11 +1,79 @@
 import "./Navbar.css"
 import { Link } from "react-router-dom"
-import SignIn from "../../../screens/SignIn/SignIn"
+import { useState, useEffect } from "react";
 
 export default function Navbar(props) {
+  const [windowDimension, setWindowDimension] = useState(null);
+  const [hamburger, setHamburger] = useState(false);
+  const [visible, setVisible] = useState(false);
+
+  // Set windowDimension to figure out which layout to render
+  useEffect(() => {
+    setWindowDimension(window.innerWidth);
+  }, []);
+
+  // If screen is resized, reset windowDimension
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimension(window.innerWidth);
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  // Control whether hamburger links are visible through handleClick
+  const handleClick = (e) => {
+    e.preventDefault();
+    setVisible(!visible);
+    setHamburger(!hamburger);
+  };
+
+  // Use isMobile to determine which layout to render
+  const isMobile = windowDimension <= 640;
   return (
-    <div className="no-auth-nav">
-      <Link to="sign-in"><SignIn /></Link>
-    </div>
+    <nav>
+      <h1>flashy</h1>
+      {isMobile ? (
+        <div className="mobile-nav">
+          <svg
+            viewBox="0 0 200 160"
+            width="40"
+            height="40"
+            className="hamburger"
+            onClick={handleClick}
+          >
+            <rect width="200" ry="20" height="30"></rect>
+            <rect y="50" ry="20" width="200" height="30"></rect>
+            <rect y="100" ry="20" width="200" height="30"></rect>
+          </svg>
+          <div
+            className="hamburger-links"
+            style={{ display: hamburger && visible ? "flex" : "none" }}
+          >
+            <div className="no-auth-nav">
+              <Link to="/sign-in">sign in</Link>
+              <Link to="/sign-up">sign up</Link>
+            </div>
+            <div className="auth-nav">
+              <Link to="/flashcards">home</Link>
+              <Link to="/flashcards/create-deck">create deck</Link>
+              <Link to="/sign-out">sign out</Link>
+            </div>
+          </div>
+          </div>
+      ): (
+          <div className="desktop-nav">
+            <div className="no-auth-nav">
+              <Link to="/sign-in">sign in</Link>
+              <Link to="/sign-up">sign up</Link>
+            </div>
+            <div className="auth-nav">
+              <Link to="/flashcards">home</Link>
+              <Link to="/flashcards/create-deck">create deck</Link>
+              <Link to="/sign-out">sign out</Link>
+            </div>
+        </div>
+      )}
+    </nav>
   )
 }
--- client/src/screens/SignUp/SignUp.jsx
+++ client/src/screens/SignUp/SignUp.jsx
@@ -1,10 +1,108 @@
 import "./SignUp.css";
 import Layout from "../../components/shared/Layout/Layout"
+import { useState } from "react";
+// import { signUp, signIn } from "../../services/users";
+// import { useHistory } from "react-router-dom";
 
 export default function SignUp(props) {
+  // const history = useHistory()
+  
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirmation: "",
+    isError: false,
+    errorMsg: ""
+  })
+
+  const handleChange = (event) => (
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value,
+    }))
+
+  // const onSignUp = (event) => {
+  //   event.preventDefault();
+  //   const { setUser } = props
+  // signUp(form)
+  // .then(() => signIn(form))
+  // .then((user) => setUser(user))
+  // .then(() => history.push("/"))
+  // .catch((error) => {
+  //   console.error(error);
+  //   setForm({
+  //     email: "",
+  //     password: "",
+  //     passwordConfirmation: "",
+  //     isError: true,
+  //     errorMsg: "Sign Up Details Invalid",
+  //   });
+  // });
+  // }
+
+  // const renderError = () => {
+  //   const toggleForm = form.isError ? "danger" : "";
+  //   if (form.isError) {
+  //     return (
+  //       <button type="submit" className={toggleForm}>
+  //         {form.errorMsg}
+  //       </button>
+  //     );
+  //   } else {
+  //     return (
+  //       <button type="submit" className="signup-submit">
+  //         sign up
+  //       </button>
+  //     );
+  //   }
+  // };
+
+  const { email, username, password, passwordConfirmation } = form;
   return (
     <Layout>
-      SIGN UP
+      <div className="signup-form">
+        <h3 className="signup-header">sign up</h3>
+        <form className="signup-form" /*onSubmit={onSignUp}*/>
+        <input
+            required
+            type="text"
+            className="signup-username"
+            name="username"
+            value={username}
+            placeholder="username"
+            onChange={handleChange}
+          />
+          <input
+            required
+            type="email"
+            className="signup-email"
+            name="email"
+            value={email}
+            placeholder="email"
+            onChange={handleChange}
+          />
+          <input
+            required
+            name="password"
+            value={password}
+            type="password"
+            className="signup-password"
+            placeholder="password"
+            onChange={handleChange}
+          />
+          <input
+            required
+            name="passwordConfirmation"
+            className="signup-password-confirmation"
+            value={passwordConfirmation}
+            type="password"
+            placeholder="confirm password"
+            onChange={handleChange}
+          />
+          {/* {renderError()} */}
+        </form>
+      </div>
     </Layout>
   )
 }
