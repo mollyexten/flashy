1f54326992d9cfb7cbe5ff0458c1611d06aa6894
mollyexten <molly.exten@gmail.com>
2021-05-01 06:35:32 -0400

    fixed type in baseURL

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.env.NODE_END === 'production' ? 'https://flashy-molly-api.herokuapp.com/' :'http://localhost:3000'
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://flashy-molly-api.herokuapp.com/' :'http://localhost:3000'
 
 const api = axios.create({
   baseURL: baseUrl
